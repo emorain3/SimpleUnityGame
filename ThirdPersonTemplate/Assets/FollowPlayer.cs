5f3204d4e952a6aa7ef55ab84910c85754108e68
emorain3 <emorain@live.com>
2018-08-24 18:12:42 -0400

    First Commit

---
+++ ThirdPersonTemplate/Assets/FollowPlayer.cs
@@ -0,0 +1,88 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+    // Camera Position and Angle
+    public Transform player_transform;
+    public Vector3 offset_cam_pos;
+
+    // Camera Angle
+    public float xSpeed = 200.0f;
+    public float ySpeed = 200.0f;
+
+    private float currentDistance;
+    private float desiredDistance;
+    private Quaternion currentRotation;
+    private Quaternion desiredRotation;
+    private Quaternion rotation;
+
+    public float mouseSensitivity = 5f;
+    public float moveSpeed = 2f;
+    public float smooth = 5.0f;
+    private float xDeg = 0.0f;
+    private float yDeg = 0.0f;
+
+    public int yMinLimit = -80;
+    public int yMaxLimit = 80;
+
+    public float zoom;
+    public float zoomSpeed = 1;
+
+    public float zoomMin = 2f;
+    public float zoomMax = 10f;
+    public float zoomDampening = 5.0f;
+
+    void Start()
+    {
+        transform.position = new Vector3(player_transform.position.x + 0.0f, player_transform.position.y + 1.5f, player_transform.position.z);
+    }
+
+
+
+    void Update() {
+        transform.position = new Vector3(player_transform.position.x + 0.0f, player_transform.position.y + 1.5f, zoom - 8);
+
+        // Smoothly tilts a transform towards a target rotation.
+        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
+        {
+            xDeg += Input.GetAxis("Mouse X") * mouseSensitivity;
+            yDeg -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+            yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
+            // set camera rotation 
+            desiredRotation = Quaternion.Euler(yDeg, xDeg, 0);
+            currentRotation = transform.rotation;
+
+            rotation = Quaternion.Lerp(currentRotation, desiredRotation, Time.deltaTime * zoomDampening);
+            transform.rotation = rotation;
+        }
+
+        CameraZoom();
+        Debug.Log("Camera Position" + transform.position + " Camera Angle: " + player_transform.rotation);
+
+    }
+
+    private static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
+    }
+    void CameraZoom() {
+        // Zooming ---- > UPDATES Z_POS ** 
+        zoom += Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+        if(zoom < zoomMin){
+            zoom = zoomMin;
+        }
+
+        if (zoom > zoomMax){
+            zoom = zoomMax;
+        }
+
+    }
+}
---
+++ ThirdPersonTemplate/Assets/PrimaryMovement.cs
@@ -0,0 +1,52 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+
+/// < NOTES> 
+/// Time.deltaTime scales _ according to my machine's frame rate
+/// </NOTES>
+
+public class PrimaryMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public int smllr_pos_update = 12;
+    public int sml_pos_update = 25;
+    public int med_pos_update = 50;
+    public int big_pos_update = 100;
+    public float forwardFore = 2000f;
+
+    void Start () {
+        Debug.Log("My first print statement");
+        rb.AddForce(0, sml_pos_update, sml_pos_update);
+	}
+
+    // Update is called once per frame
+    void Update() {
+    
+    }
+
+    // FixedUpdate is used for objects with physics
+    void FixedUpdate () {
+        
+        if (Input.GetKey("w")) {
+            rb.AddForce(0, 0, smllr_pos_update); 
+        }
+
+        if (Input.GetKey("s")) {
+            rb.AddForce(0, 0, -smllr_pos_update);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-smllr_pos_update , 0, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(smllr_pos_update, 0, 0);
+        }
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, sml_pos_update, 0);
+        }
+    }
+}
